63d958dd7b64a109203131fed722bd98427cb5b1
Valentín Vera <valentinvera2805@gmail.com>
2024-10-27 17:33:48 -0300

    feat: Add home page with all its components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,116 @@
-import Image from "next/image";
+import Header from '@/components/Header';
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <Header />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <Link
+        href="/"
+        className="my-5 ml-10 flex h-[107px] w-[219px] items-center"
+      >
+        <Image
+          src="/images/icons/Logo.svg"
+          alt="Logo of the project"
+          width={219}
+          height={107}
+        />
+      </Link>
+
+      <div className="relative mb-14 flex flex-col items-center justify-center">
+        <Image
+          src="/images/Hero.jpg"
+          className="relative h-[400px] w-full object-cover"
+          alt="Hero image"
+          width={1366}
+          height={400}
+        />
+        <div className="absolute top-[155px] flex flex-col items-center text-center">
+          <h1 className="mb-20 text-[56px] font-light text-white">
+            Toma de exámenes
+          </h1>
+          <p className="text-base font-normal text-white">
+            Nos preocupamos de brindar el mejor servicio
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </div>
+
+      <article className="mb-40 flex items-center justify-center gap-6 max-lg:mb-20 max-lg:flex-col">
+        <div className="flex rounded-[4px] border border-solid border-[#0000002D] max-lg:flex-col">
           <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/images/DoctorTyping.svg"
+            className="rounded-[4px] max-lg:h-[300px] max-lg:w-[400px] max-lg:object-cover"
+            alt="Doctor typing on a notebook keyboard"
+            width={250}
+            height={375}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          <div className="flex flex-col pl-4 pr-[110px] pt-4 max-lg:pb-4">
+            <h2 className="mb-2 text-xl font-medium text-[#212529]">
+              Historial médico
+            </h2>
+            <p className="mb-4 text-base font-normal text-[#212529]">
+              Ver detalles del historial médico
+            </p>
+            <button className="h-[38px] w-[109px] rounded-[4px] bg-[#0D6EFD] px-3 py-[6px] text-center text-white">
+              Ver historial
+            </button>
+          </div>
+        </div>
+
+        <div className="flex rounded-[4px] border border-solid border-[#0000002D] max-lg:flex-col">
           <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/images/DoctorSealing.svg"
+            className="rounded-[4px] max-lg:h-[300px] max-lg:w-full max-lg:object-cover"
+            alt="Doctor typing on a notebook keyboard"
+            width={250}
+            height={375}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          <div className="flex flex-col pl-4 pr-[110px] pt-4 max-lg:pb-4">
+            <h2 className="mb-2 text-xl font-medium text-[#212529]">
+              Exámenes
+            </h2>
+            <p className="mb-4 text-base font-normal text-[#212529]">
+              Ver resultados de exámenes realizados
+            </p>
+            <button className="h-[38px] w-[127px] rounded-[4px] bg-[#0D6EFD] px-3 py-[6px] text-center text-white">
+              Ver exámenes
+            </button>
+          </div>
+        </div>
+      </article>
+
+      <footer className="bg-[#950DFC]">
+        <article className="mb-16 ml-40 flex gap-10 pt-10 max-lg:ml-8">
+          <div className="flex flex-col">
+            <h3 className="mb-4 text-xl font-medium text-white">Servicios</h3>
+            <Link href="" className="text-base font-normal text-white">
+              Consultas médicas
+            </Link>
+            <Link href="" className="text-base font-normal text-white">
+              Exámenes médicos
+            </Link>
+            <Link href="" className="text-base font-normal text-white">
+              Resultados exámenes
+            </Link>
+          </div>
+
+          <div className="flex flex-col">
+            <h3 className="mb-4 text-xl font-medium text-white">Contacto</h3>
+            <p className="text-base font-normal text-white">+569 251531851</p>
+            <p className="text-base font-normal text-white">
+              contacto@centrocousin.cl
+            </p>
+          </div>
+        </article>
+
+        <p className="pb-4 text-center text-[13px] font-normal text-white">
+          ©Centro médico Cousin todos los derechos reservados
+        </p>
       </footer>
-    </div>
+    </>
   );
-}
+};
+
+export default Home;
