c9f0b01e9f4d281d077575aae2289414b0be490e
Valentín Vera <valentinvera2805@gmail.com>
2024-11-06 11:04:10 -0300

    feat: Add new id property to each object/doctor and change the language of a property from spanish to english

--- src/data/list-docts.ts
+++ src/data/list-docts.ts
@@ -1,32 +1,38 @@
 export const listDocts = [
   {
+    id: 1,
     name: 'Dra. Amanda Pozo',
-    especialidad: 'Cardiología',
+    specialty: 'Cardiología',
     img: '/images/doctors/docPozo.svg',
   },
   {
+    id: 2,
     name: 'Dr. Lucas Peña',
-    especialidad: 'Nefrología',
+    specialty: 'Nefrología',
     img: '/images/doctors/docPeña.svg',
   },
   {
+    id: 3,
     name: 'Dr. Mario Estevez',
-    especialidad: 'Traumatólogo',
+    specialty: 'Traumatólogo',
     img: '/images/doctors/docEstevez.svg',
   },
   {
+    id: 4,
     name: 'Dra. Melany Villa',
-    especialidad: 'Pediatría',
+    specialty: 'Pediatría',
     img: '/images/doctors/docVilla.svg',
   },
   {
+    id: 5,
     name: 'Dr. Angel Passini',
-    especialidad: 'Dermatología',
+    specialty: 'Dermatología',
     img: '/images/doctors/docPassini.svg',
   },
   {
+    id: 6,
     name: 'Dra. Amanda Pozo',
-    especialidad: 'Neumología',
+    specialty: 'Neumología',
     img: '/images/doctors/docPozo.svg',
   },
 ];
