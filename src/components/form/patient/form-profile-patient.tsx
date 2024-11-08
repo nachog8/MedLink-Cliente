459440eb34d4196e388f29e96eef3ffc0b3d860a
JamesBarr456 <barretosantiago456@hotmail.com>
2024-11-08 00:13:41 -0300

    modificacion p1

--- src/components/form/patient/form-profile-patient.tsx
+++ src/components/form/patient/form-profile-patient.tsx
@@ -40,6 +40,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { useEffect, useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
@@ -50,29 +51,26 @@ import { cn } from '@/lib/utils';
 import { edithProfilePatientAction } from '@/actions/patient-actions';
 import { format } from 'date-fns';
 import { toast } from '@/hooks/use-toast';
-import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useFormState } from 'react-dom';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function EditProfileForm() {
   const [state, formAction] = useFormState(edithProfilePatientAction, null);
+  const [fileName, setFileName] = useState<string | null>(null);
   const form = useForm<EditProfileFormType>({
     resolver: zodResolver(editProfileSchema),
     defaultValues: {
-      name: 'blanca',
-      last_name: 'barreto',
-      birth_date: new Date('1986-02-03'),
+      firstName: 'blanca',
+      lastName: 'barreto',
+      birthDate: new Date('1986-02-03'),
       genre: 'female',
-      about_me:
+      aboutMe:
         'Comprometida con el seguimiento de mi bienestar y la consulta de información médica en tiempo real, facilitando la comunicación y el intercambio de información con profesionales de la salud. Mi objetivo es gestionar de manera proactiva mi salud y mantener un registro actualizado para una mejor atención médica.',
       phone: '+54-3718-441861',
       email: 'barrett@hotmail.com',
-      province: 'Corrientes',
-      city: 'Corrientes',
-      avatar_image:
-        'https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?q=80&w=256&h=256&fit=crop',
-      banner_image: '',
+      location: 'Corrientes, Corrientes',
+      avatar: undefined,
       height: '175 cm',
       weight: '70 kg',
       bloodType: 'O+',
@@ -102,20 +100,21 @@ export default function EditProfileForm() {
       });
     }
   }, [state]);
+
   return (
     <Form {...form}>
       <form action={formAction} className="space-y-5 px-3">
         <FormField
           control={form.control}
-          name="name"
+          name="firstName"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Nombre</FormLabel>
               <FormControl>
                 <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
+                  <Input {...field} className="pl-10" />
                   <User
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
+                    className="absolute left-3 top-1/2 -translate-y-1/2"
                     size={18}
                   />
                 </div>
@@ -126,15 +125,15 @@ export default function EditProfileForm() {
         />
         <FormField
           control={form.control}
-          name="last_name"
+          name="lastName"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Apellido</FormLabel>
               <FormControl>
                 <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
+                  <Input {...field} className="pl-10" />
                   <User
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
+                    className="absolute left-3 top-1/2 -translate-y-1/2"
                     size={18}
                   />
                 </div>
@@ -145,7 +144,7 @@ export default function EditProfileForm() {
         />
         <FormField
           control={form.control}
-          name="birth_date"
+          name="birthDate"
           render={({ field }) => (
             <FormItem className="flex flex-col">
               <FormLabel>Fecha de Nacimiento</FormLabel>
@@ -155,7 +154,7 @@ export default function EditProfileForm() {
                     <Button
                       variant={'outline'}
                       className={cn(
-                        'relative w-[240px] bg-sky-50 pl-10 text-left font-normal',
+                        'relative w-[240px] pl-10 text-left font-normal',
                         !field.value && 'text-muted-foreground'
                       )}
                     >
@@ -165,7 +164,7 @@ export default function EditProfileForm() {
                         <span>Elige una Fecha</span>
                       )}
                       <CalendarIcon
-                        className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
+                        className="absolute left-3 top-1/2 -translate-y-1/2"
                         size={18}
                       />
                     </Button>
@@ -195,7 +194,7 @@ export default function EditProfileForm() {
               <FormLabel>Genre</FormLabel>
               <Select onValueChange={field.onChange} defaultValue={field.value}>
                 <FormControl>
-                  <SelectTrigger className="bg-sky-50">
+                  <SelectTrigger className="">
                     <SelectValue placeholder="Select a genre" />
                   </SelectTrigger>
                 </FormControl>
@@ -211,12 +210,12 @@ export default function EditProfileForm() {
         />
         <FormField
           control={form.control}
-          name="about_me"
+          name="aboutMe"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Sobre mi</FormLabel>
               <FormControl>
-                <Textarea {...field} className="h-32 bg-sky-50" />
+                <Textarea {...field} className="h-32" />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -230,9 +229,9 @@ export default function EditProfileForm() {
               <FormLabel>Telefono</FormLabel>
               <FormControl>
                 <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
+                  <Input {...field} className="pl-10" />
                   <Phone
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
+                    className="absolute left-3 top-1/2 -translate-y-1/2"
                     size={18}
                   />
                 </div>
@@ -249,9 +248,9 @@ export default function EditProfileForm() {
               <FormLabel>Email</FormLabel>
               <FormControl>
                 <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
+                  <Input {...field} className="pl-10" />
                   <Mail
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
+                    className="absolute left-3 top-1/2 -translate-y-1/2"
                     size={18}
                   />
                 </div>
@@ -262,15 +261,15 @@ export default function EditProfileForm() {
         />
         <FormField
           control={form.control}
-          name="province"
+          name="location"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Provincia</FormLabel>
+              <FormLabel>Locación</FormLabel>
               <FormControl>
                 <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
+                  <Input {...field} className="pl-10" />
                   <MapPin
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
+                    className="absolute left-3 top-1/2 -translate-y-1/2"
                     size={18}
                   />
                 </div>
@@ -281,55 +280,47 @@ export default function EditProfileForm() {
         />
         <FormField
           control={form.control}
-          name="city"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Ciudad</FormLabel>
-              <FormControl>
-                <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
-                  <MapPin
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
-                    size={18}
-                  />
-                </div>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="avatar_image"
-          render={({ field }) => (
+          name="avatar"
+          render={({ field: { value, onChange, ...field } }) => (
             <FormItem>
               <FormLabel>Imagen de Avatar</FormLabel>
               <FormControl>
-                <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
-                  <ImageIcon
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
-                    size={18}
-                  />
-                </div>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="banner_image"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Imagen para Banner</FormLabel>
-              <FormControl>
-                <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
-                  <ImageIcon
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
-                    size={18}
-                  />
+                <div className="flex items-center space-x-4">
+                  <div className="relative">
+                    <Input
+                      {...field}
+                      type="file"
+                      accept="image/*"
+                      onChange={(event) => {
+                        const file = event.target.files?.[0];
+                        if (file) {
+                          onChange(file);
+                          setFileName(file.name);
+                        }
+                      }}
+                      className="hidden"
+                      id="avatar-upload"
+                    />
+                    <Button
+                      type="button"
+                      variant="outline"
+                      className="pl-10"
+                      onClick={() =>
+                        document.getElementById('avatar-upload')?.click()
+                      }
+                    >
+                      Seleccionar Archivo
+                    </Button>
+                    <ImageIcon
+                      className="absolute left-3 top-1/2 -translate-y-1/2"
+                      size={18}
+                    />
+                  </div>
+                  {fileName && (
+                    <span className="text-sm text-muted-foreground">
+                      {fileName}
+                    </span>
+                  )}
                 </div>
               </FormControl>
               <FormMessage />
@@ -344,9 +335,9 @@ export default function EditProfileForm() {
               <FormLabel>Altura</FormLabel>
               <FormControl>
                 <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
+                  <Input {...field} className="pl-10" />
                   <Ruler
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
+                    className="absolute left-3 top-1/2 -translate-y-1/2"
                     size={18}
                   />
                 </div>
@@ -363,9 +354,9 @@ export default function EditProfileForm() {
               <FormLabel>Peso</FormLabel>
               <FormControl>
                 <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
+                  <Input {...field} className="pl-10" />
                   <Weight
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
+                    className="absolute left-3 top-1/2 -translate-y-1/2"
                     size={18}
                   />
                 </div>
@@ -382,9 +373,9 @@ export default function EditProfileForm() {
               <FormLabel>Tipo de Sangre</FormLabel>
               <FormControl>
                 <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
+                  <Input {...field} className="pl-10" />
                   <Droplet
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
+                    className="absolute left-3 top-1/2 -translate-y-1/2"
                     size={18}
                   />
                 </div>
@@ -401,9 +392,9 @@ export default function EditProfileForm() {
               <FormLabel>Tendencia de Presión Arterial</FormLabel>
               <FormControl>
                 <div className="relative">
-                  <Input {...field} className="bg-sky-50 pl-10" />
+                  <Input {...field} className="pl-10" />
                   <Heart
-                    className="absolute left-3 top-1/2 -translate-y-1/2 text-sky-400"
+                    className="absolute left-3 top-1/2 -translate-y-1/2"
                     size={18}
                   />
                 </div>
@@ -416,13 +407,13 @@ export default function EditProfileForm() {
           control={form.control}
           name="isDonor"
           render={({ field }) => (
-            <FormItem className="flex flex-row items-center justify-between rounded-lg border bg-green-200 p-4">
+            <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
               <div className="space-y-0.5">
-                <FormLabel className="flex gap-3 text-green-800">
-                  <HeartHandshake className="text-green-400" size={18} />
+                <FormLabel className="flex gap-3">
+                  <HeartHandshake size={18} />
                   Donador de Organos
                 </FormLabel>
-                <FormDescription className="text-green-800">
+                <FormDescription>
                   ¿Estás registrado como donante de órganos?
                 </FormDescription>
               </div>
@@ -439,13 +430,13 @@ export default function EditProfileForm() {
           control={form.control}
           name="hasHealthyLifestyle"
           render={({ field }) => (
-            <FormItem className="flex flex-row items-center justify-between rounded-lg border bg-green-200 p-4">
+            <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
               <div className="space-y-0.5">
-                <FormLabel className="flex gap-3 text-green-800">
-                  <Bike className="text-green-400" size={18} />
+                <FormLabel className="flex gap-3">
+                  <Bike className="" size={18} />
                   Estilo de Vida
                 </FormLabel>
-                <FormDescription className="text-green-800">
+                <FormDescription>
                   ¿Mantienes un estilo de vida saludable?
                 </FormDescription>
               </div>
@@ -462,13 +453,13 @@ export default function EditProfileForm() {
           control={form.control}
           name="hasAllergies"
           render={({ field }) => (
-            <FormItem className="flex flex-row items-center justify-between rounded-lg border bg-orange-400 p-4">
+            <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
               <div className="space-y-0.5">
-                <FormLabel className="flex gap-3 text-orange-800">
-                  <CircleAlert className="text-orange-700" size={18} />
+                <FormLabel className="flex gap-3">
+                  <CircleAlert size={18} />
                   Alergias
                 </FormLabel>
-                <FormDescription className="text-orange-800">
+                <FormDescription>
                   Posees alguna alergia a medicamentos u de otro tipo?
                 </FormDescription>
               </div>
@@ -485,13 +476,13 @@ export default function EditProfileForm() {
           control={form.control}
           name="hasChronicDiseases"
           render={({ field }) => (
-            <FormItem className="flex flex-row items-center justify-between rounded-lg border bg-orange-400 p-4">
+            <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
               <div className="space-y-0.5">
-                <FormLabel className="flex gap-3 text-orange-800">
-                  <CircleAlert className="text-orange-700" size={18} />
+                <FormLabel className="flex gap-3">
+                  <CircleAlert size={18} />
                   Enfermedades Cronicas
                 </FormLabel>
-                <FormDescription className="text-orange-800">
+                <FormDescription>
                   Tienes alguna enfermedad cronica?
                 </FormDescription>
               </div>
